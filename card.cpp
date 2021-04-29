93e90be19852b7c2f22e9087743848f9734092ce
gjohns68 <71344108+gjohns68@users.noreply.github.com>
2021-04-29 09:39:54 -0400

    Add files via upload
    reupload of original files

---
+++ card.cpp
@@ -0,0 +1,67 @@
+#include "card.h"
+
+Color Card::getColor() const{
+    return color;
+}
+    
+int Card::getNumber() const{
+    return number;
+}
+
+void Card::setColor(Color c){
+    color = c;
+}
+
+void Card::setNumber(int n){
+    number = n;
+}
+
+NumberCard::NumberCard(Color c, int n) {
+    setColor(c);
+    setNumber(n);
+}
+
+string NumberCard::render(int line) const{
+    stringstream ss;
+    switch(line){
+        case 0: return ".___________.";
+        case 1: return "|           |";
+        case 2: 
+            switch(color){
+            case RED:
+                return "|    RED    |";
+                break;
+            case BLUE:
+                return "|    BLUE   |";
+                break;
+            case GREEN:
+                return "|   GREEN   |";
+                break;
+            case YELLOW:
+                return "|  YELLOW   |";
+                break;
+            default:
+                return "|           |";
+                break;
+            break;
+            }
+        case 3:
+            ss << "|     " <<  number << "     |";
+            return ss.str();
+            break;
+        case 4: return "|           |";
+        case 5: return "|           |";
+        case 6: return "|           |";
+        case 7: return "|___________|";
+        default:
+            return " ";
+    }
+}
+
+bool NumberCard::play(Card* discard, GameState &gameState) {
+    if(color == discard->getColor() || number == discard->getNumber()){
+        return true;
+    } else {
+        return false;
+    }
+}
